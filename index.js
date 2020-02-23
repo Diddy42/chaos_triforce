4e55c260493a1f4066c129a20b71e4e9ed1e99ed
Nadir <nadir.casciola@gmail.com>
2020-02-23 01:24:56 +0100

    y

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
-function clicked_button(){
-    alert('you clicked!');
+var x_points = [];
+var y_points = [];
+
+var p_layout = {
+    //xaxis: {range: [-1, 51]},
+    //yaxis: {range: [-1, 51]}
+};
+
+function on_start(){
+    var chart = document.getElementById('chart');
+
+    var data = [{
+        x: [],
+        y: [],
+        mode: 'markers'
+      }];
+
+    Plotly.newPlot(chart, data, p_layout);
+}
+
+function plot_next_point(){
+    var last_x = x_points[x_points.length - 1];
+    var last_y = y_points[y_points.length - 1];
+
+    var r = Math.floor(Math.random() * 3);
+
+    var target_x = x_points[r];
+    var target_y = y_points[r];
+
+    var nx = last_x + (target_x - last_x)/2;
+    var ny = last_y + (target_y - last_y)/2;
+
+    draw_point(nx, ny);
+}
+
+function plot_the_next_n_points(n){
+    for(i = 0; i < n; i++){
+        plot_next_point();
+    }
+}
+
+function draw_point(xp, yp){
+    x_points.push(xp);
+    y_points.push(yp);
+
+    var chart = document.getElementById('chart');
+
+    var data = [{
+        x: x_points,
+        y: y_points,
+        mode: 'markers'
+      }];
+
+    Plotly.newPlot(chart, data, p_layout);
+}
+
+function draw_n_points(n){
+    for(i = 0; i < n; i++){
+        draw_point(Math.floor(Math.random() * 50), Math.floor(Math.random() * 50));
+    }
 }
